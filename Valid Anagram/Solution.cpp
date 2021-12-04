d1b93f81e2484ab7cdd0947c4bc603d164d46d7b
YeEun_Choi <89172314+rosy-y@users.noreply.github.com>
2021-12-04 10:45:25 +0900

    Update Solution.cpp
    242> Valid Anagram

--- Valid Anagram/Solution.cpp
+++ Valid Anagram/Solution.cpp
@@ -1,1 +1,15 @@
-int a;
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
+        
+        unordered_map<char, int> m;
+            for (int i=0; i<s.size(); i++) {
+                m[s[i]]++;
+                m[t[i]]--;
+        }
+        for (auto find : m)
+            if (find.second) return false;  // value 값이 있으면 false
+        return true;
+    } 
+};
