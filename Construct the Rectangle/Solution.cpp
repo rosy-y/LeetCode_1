6fd41aafdf669b28c90d8efed5504c6089ecc766
YeEun_Choi <89172314+rosy-y@users.noreply.github.com>
2021-12-24 11:05:31 +0900

    Create Solution.cpp
    492> Construct the Rectangle

---
+++ Construct the Rectangle/Solution.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> constructRectangle(int area) {
+        int w = sqrt(area);
+        
+        while (area % w) w--;
+        return {area/w, w};
+    }
+};
